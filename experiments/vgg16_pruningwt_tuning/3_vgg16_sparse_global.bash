e0b9b1312b5b0b2a377da328ea0fa01224bd9164
levaid <levaid@vivaldi.net>
2020-09-15 10:04:03 +0200

    added vgg16 experiments

---
+++ experiments/vgg16_pruningwt_tuning/3_vgg16_sparse_global.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES=2
+strategy='sparse_global'
+
+for r in 1 2 3 4; do
+        python ../../open_lth.py lottery --model_name=cifar_vgg_16 --levels=13 --dataset_name=cifar10 --batch_size=128  --model_init=kaiming_normal --batchnorm_init=uniform --optimizer_name=sgd --lr=0.1 --momentum=0.9 --training_steps=100ep --pruning_strategy=$strategy --pruning_fraction=0.5 --replicate=$r --milestone_steps=60ep --gamma=0.1 --weight_decay=0.0001 --num_workers=1;
+done
